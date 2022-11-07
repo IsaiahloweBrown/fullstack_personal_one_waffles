0c9a5850106c6dd904f849acb4d5983260f968ba
Isaiah Lowe-Brown <isaiahlowebrown@gmail.com>
2022-11-06 23:55:18 -0500

    first commit

---
+++ config/database.js
@@ -0,0 +1,12 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://sixers:ulAmPUYkMt7MxZOM@cluster0.srifgsf.mongodb.net/waffles?retryWrites=true&w=majority', 
+    'dbName': 'waffles',
+    
+};
+
+
+
+// 'url' : 'mongodb+srv://demo:demo@cluster0.q2ojb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+//     'dbName': 'demo'
---
+++ public/main.js
@@ -0,0 +1,113 @@
+var cart = document.getElementsByClassName("fa-shopping-cart");
+var notDelivered = document.getElementsByClassName("fa-exclamation-circle");
+var trash = document.getElementsByClassName("fa-trash-o");
+
+Array.from(cart).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const sizes = this.parentNode.parentNode.childNodes[3].innerText
+        const styles = this.parentNode.parentNode.childNodes[5].innerText
+        const sides = this.parentNode.parentNode.childNodes[7].innerText
+        const status = this.parentNode.parentNode.childNodes[9].innerText
+        fetch('orders', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'name': name,
+            'sizes': sizes,
+            'styles': styles,
+            'sides': sides,
+            'status': status
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+      });
+});
+
+Array.from(notDelivered).forEach(function(element) {
+  element.addEventListener('click', function(){
+    const name = this.parentNode.parentNode.childNodes[1].innerText
+    const sizes = this.parentNode.parentNode.childNodes[3].innerText
+    const styles = this.parentNode.parentNode.childNodes[5].innerText
+    const sides = this.parentNode.parentNode.childNodes[7].innerText
+    const status = this.parentNode.parentNode.childNodes[9].innerText
+    fetch('dislikes', {
+      method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'name': name,
+        'sizes': sizes,
+        'styles': styles,
+        'sides': sides,
+        'status': status
+      })
+    })
+    .then(response => {
+      if (response.ok) return response.json()
+    })
+    .then(data => {
+      console.log(data)
+      window.location.reload(true)
+    })
+  });
+});
+
+// Array.from(notDelivered).forEach(function(element) {
+//       element.addEventListener('click', function(){
+//         const name = this.parentNode.parentNode.childNodes[1].innerText
+//         const msg = this.parentNode.parentNode.childNodes[3].innerText
+//         //add phone number
+//         const cart = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+//         const notDelivered = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+//         console.log(notDelivered)
+//         //fetches to api created in routes
+//         //a request
+//         fetch('dislikes', {
+//           method: 'put',
+//           headers: {'Content-Type': 'application/json'},
+//           body: JSON.stringify({
+//             'name': name,
+//             'msg': msg,
+//             'cart': cart
+//           })
+//         })
+//         .then(response => {
+//           if (response.ok) return response.json()
+//         })
+//         .then(data => {
+//           console.log(data)
+//           window.location.reload(true)
+//         })
+//       });
+// });
+
+
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const sizes = this.parentNode.parentNode.childNodes[3].innerText
+        const styles = this.parentNode.parentNode.childNodes[5].innerText
+        const sides = this.parentNode.parentNode.childNodes[7].innerText
+        fetch('orders', {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'name': name,
+            'sizes': sizes,
+            'styles': styles,
+            'sides': sides,
+         
+          })
+        }).then(function (response) {
+          window.location.reload()
+        })
+      });
+});
